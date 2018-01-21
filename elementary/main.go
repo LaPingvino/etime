e5c407b797593d13117b376b5d7b5affe05d51a9
Joop Kiefte <ikojba@gmail.com>
2018-01-21 21:51:27 +0100

    add reference disk

--- elementary/main.go
+++ elementary/main.go
@@ -19,5 +19,4 @@ func main() {
 	clock := dom.GetWindow().Document().GetElementByID("clock")
 	explain := dom.GetWindow().Document().GetElementByID("explain")
 	timer(clock)
-	explain.SetInnerHTML("Fire, Air, Water and Earth are used in a cycle to provide four blocks of six hours each. This time is the same everywhere on earth. It was created to give a similar idea of time everywhere in the world without relying on morning/evening etc in one specific location. In your timezone, midnight is at "+etime.Descriptive("0")+ ", morning is at "+etime.Descriptive("6")+", noon is at "+etime.Descriptive("12")+ " and evening is at "+etime.Descriptive("18")+". A more visual representation is coming soon!")
 }
